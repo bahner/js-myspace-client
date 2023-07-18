7e5e1758a2288634ce9bc917cc61f381ed7875f9
Lars Bahner <lars.bahner@gmail.com>
2023-07-18 14:56:10 +0200

    Websocket now can write async

--- src/index.js
+++ src/index.js
@@ -1,20 +1,42 @@
-import { initializeTerminal, log } from './terminal.js';
+import { initializeTerminal, log, readLine } from './terminal.js';
 
 import { attachListeners } from './listeners.js';
 import { startLibp2p } from './libp2p.js';
 
+const topicBaseUrl = 'ws://localhost:5002/api/v0/topics';
+// const topicBaseUrl = 'ws://localhost:12345/api/v0/topics';
+const defaultTopic = 'myspace';
+
+function getTopicUrl(topic) {
+  return `${topicBaseUrl}/${topic}`;
+}
+
+function setTopic(topic) {
+  const url = getTopicUrl(topic);
+  return initializeTerminal(url);
+}
 async function main() {
-  const { terminal, fitAddon } = initializeTerminal();
-
-  function updateTerminal(txt) {
-    log(txt, log);
+  const { xterm, fitAddon} = await setTopic(defaultTopic);
+  
+  fitAddon.fit();
+  
+  window.onresize = (evt) => {
+    fitAddon.fit()
   }
+  // function updateTerminal(txt) {
+  //   log(txt, log);
+  // }
 
-  const libp2p = await startLibp2p();
-  attachListeners(libp2p, updateTerminal);
+  // const libp2p = await startLibp2p();
+  // // attachListeners(libp2p, updateTerminal);
 
-  log('libp2p started!');
-  log(`libp2p id is ${libp2p.peerId.toString()}`);
+  // log('libp2p started!');
+  // log(`libp2p id is ${libp2p.peerId.toString()}`);
+  // xterm.writeln(`libp2p id is ${libp2p.peerId.toString()}`);
+  xterm.writeln(`Hello ${defaultTopic}!`);
+
+  readLine();
+
 }
 
 main();
--- src/terminal.js
+++ src/terminal.js
@@ -1,18 +1,70 @@
 /* eslint-disable no-console */
+import { AttachAddon } from 'xterm-addon-attach';
 import { FitAddon } from 'xterm-addon-fit';
+import { Readline } from "xterm-readline";
 import { Terminal } from 'xterm';
+import { WebLinksAddon } from 'xterm-addon-web-links';
 
-let terminal;
+let topicSocket;
+let xterm;
+let attachAddon;
+let fitAddon;
+let readlineAddon;
 
-export function initializeTerminal() {
-  terminal = new Terminal();
-  const fitAddon = new FitAddon();
-  terminal.loadAddon(fitAddon);
-  terminal.open(document.getElementById('terminal'));
-  return { terminal, fitAddon };
+const options = {
+  cursorBlink: true,
+  cursorStyle: 'underline',
+};
+
+export function initializeTerminal(socketUrl) {
+  return new Promise((resolve, reject) => {
+    if (topicSocket) {
+      topicSocket.close();
+    }
+
+    xterm = new Terminal(options);
+    topicSocket = new WebSocket(socketUrl);
+
+    topicSocket.onopen = function (event) {
+      console.debug("WebSocket is open now.");
+
+      fitAddon = new FitAddon();
+      xterm.loadAddon(fitAddon);
+
+      xterm.loadAddon(new WebLinksAddon());
+
+      readlineAddon = new Readline();
+      xterm.loadAddon(readlineAddon); // Load readlineAddon into xterm
+
+      xterm.open(document.getElementById('terminal'));
+
+      // Resolve the promise with the xterm, fitAddon, and readlineAddon instances
+      resolve({ xterm, fitAddon, readlineAddon });
+    };
+
+    topicSocket.onclose = function (event) {
+      console.debug("WebSocket is closed now.", event);
+    };
+
+    topicSocket.onerror = function (event) {
+      console.log("WebSocket encountered error: ", event);
+      reject(event);  // Reject the promise if there's an error
+    };
+  });
 }
 
 export function log(txt) {
   console.info(txt);
-  terminal.writeln(txt);
+  xterm.writeln(txt);
+}
+
+export function readLine() {
+  readlineAddon.read(">")
+    .then(processLine);
+}
+
+function processLine(text) {
+  readlineAddon.println("you entered: " + text);
+  topicSocket.send(text);
+  setTimeout(readLine);
 }
